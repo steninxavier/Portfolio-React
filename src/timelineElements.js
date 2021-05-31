447002e90fcea0f4086b545dd5e634491ff30ec6
Stenin Xavier <67514601+steninxavier@users.noreply.github.com>
2021-06-01 01:50:27 +0200

    Add files via upload

---
+++ src/timelineElements.js
@@ -0,0 +1,52 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Web Development Internship(Remote)",
+    location: "LastMile UG, Frankfurt (www.thelastmile.shop)",
+    description:
+      "Website designing, building and maintaining.",
+     description1:"Directing or performing Website updates.",
+     description2:"Using scripting or authoring languages, management tools, content creation tools.",
+     description3:"Making of Responsive design.",
+     description4:"Testing the code of the website and update the backlogs in jira.",
+    date: "03/2021 - 05/2021",
+    icon: "work",
+  },
+  {
+    id: 2,
+    title: "Web Development and Digital Marketing",
+    location: "Freelancer, India",
+    description:
+      "Website building based on client's requirements.",
+     description1:"Website creation with Content Management Systems like Wordpress.",
+     description2:"Building Dynamic websites with HTML, CSS, Javascript.",
+     description3:"Responsive web designing with bootstrap framework.",
+     description4:"Plan and execute all digital marketing, including SEO/SEM, marketing database, email.",
+     description5:"Design, build and maintain social media presence.",
+     description6:"Identify trends and insights, and optimize spend and performance based on the insights.",
+    date: "07/2016 - 12/2017",
+    icon: "work",
+  },
+  
+  {
+    id: 4,
+    title: "Btech in Computer Science",
+    location: "Mahathma Gandhi University, India",
+    
+  
+    date: "06/2012 - 06/2016",
+    icon: "school",
+  },
+  {
+    id: 5,
+    title: "Global MBA",
+    location: "Berlin School Of Business And Innovation, Berlin",
+    
+    
+    date: "05/2019 - 05/2021",
+    icon: "school",
+  },
+  
+];
+
+export default timelineElements;
